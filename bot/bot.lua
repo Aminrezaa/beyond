961363961388183816615751507477533ba7c964
Aminrezaa <aminrezaaslani1382@gmail.com>
2017-01-26 01:01:49 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -83,7 +83,7 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {157059515},
+    sudo_users = {219354110},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
